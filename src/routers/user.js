28f4c57f5a7ee17d9ffc16a017807436300484f8
pratibha.h <pratibha.h@innovify.in>
2020-10-08 19:29:01 +0530

    auth module, logout, login api's added

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
-
+const auth = require("../middleware/auth")
 const router = express.Router();
-router.post('/users', async (req, res) => {
+router.post('/users/signUp', async (req, res) => {
     const user = new User(req.body);
     try {
         const token = await user.generateAuthToken();
@@ -14,6 +14,36 @@ router.post('/users', async (req, res) => {
     } catch (e) {
         res.status(401).send(e)
     }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        });
+        await req.user.save();
+        res.send({
+            user: req.user,
+            "message": "User Logged Out Successfully !"
+        })
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send({
+            user: req.user,
+            "message": "User Logged Out Successfully of All Devices!"
+        })
+    } catch (e) {
+        console
+        res.status(500).send()
+    }
+
 })
 
 router.post('/users/login', async (req, res) => {
@@ -29,13 +59,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
