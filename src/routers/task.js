a74157abbd5e1db31dbb0f86a9281ff4b69b419d
pratibha.h <pratibha.h@innovify.in>
2020-10-09 12:54:29 +0530

    auth added in task api

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const Task = require("../models/task");
+const auth = require("../middleware/auth")
 
 const router = express.Router();
 
-router.post('/tasks', async (req, res) => {
-    const tasks = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const tasks = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
     try {
         await tasks.save();
         res.status(201).send(tasks)
@@ -13,10 +17,12 @@ router.post('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
 
     const { completed, limit, sortBy, pageNum } = req.query;
-    const match = {};
+    const match = {
+        owner: req.user._id
+    };
     let sort = {};
     let limitNumeric;
     let skip;
@@ -38,12 +44,11 @@ router.get('/tasks', async (req, res) => {
     try {
         res.send(tasks);
     } catch (e) {
-        console.log(e)
         res.status(500).send()
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
         const task = await Task.findById(_id);
@@ -56,7 +61,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
         const task = await Task.findById(_id);
@@ -72,10 +77,10 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
-        const task = await Task.findByIdAndDelete(_id, req.body);
+        const task = await Task.findByIdAndDelete(_id);
         if (!task) {
             return res.status(404).send()
         }
--- src/routers/user.js
+++ src/routers/user.js
@@ -63,45 +63,24 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            res.status(201).send();
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
-router.patch('/users/:id', auth, async (req, res) => {
-    const _id = req.params.id;
+router.patch('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send()
-        }
         const objKeys = Object.keys(req.body);
-        objKeys.forEach((param) => user[param] = req.body[param]);
-        await user.save();
-        res.send(user)
+        objKeys.forEach((param) => req.user[param] = req.body[param]);
+        await req.user.save();
+        res.send(req.user)
     } catch (e) {
         res.status(401).send()
     }
 })
 
-router.delete('/users/:id', auth, async (req, res) => {
-    const _id = req.params.id;
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(_id, req.body);
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.status(202).send(task)
+        await req.user.remove();
+        res.status(200).send(req.user);
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send();
     }
 })
 
